257701b3a1d921795e937bd56cd705a654a7e9c2
Yuriy Izbash <izbash91@gmail.com>
2026-02-03 16:43:28 +0100

    Added Initial Presentation Files

---
+++ Post Viewer/Presentation/Feed/View/PostCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  PostCollectionViewCell.swift
+//  Post Viewer
+//
+//  Created by yuriy on 3. 2. 26.
+//
+
+import UIKit
+
+@MainActor
+final class PostCollectionViewCell: UICollectionViewCell {
+    
+}
