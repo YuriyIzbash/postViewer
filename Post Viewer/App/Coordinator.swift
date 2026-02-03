cc7715f7d88f3ee629922e022d4504e8afd07f90
Yuriy Izbash <izbash91@gmail.com>
2026-02-03 17:43:52 +0100

    Added Feed and PostDetail to Coordinator

--- Post Viewer/App/Coordinator.swift
+++ Post Viewer/App/Coordinator.swift
@@ -10,6 +10,7 @@ import UIKit
 @MainActor
 protocol Coordinator: AnyObject {
     var navigationController: UINavigationController { get set }
+    func start()
 }
 
 @MainActor
@@ -19,15 +20,29 @@ protocol PostListCoordinator: Coordinator {
 
 @MainActor
 final class MainCoordinator: PostListCoordinator {
-    func showPostDetail(for viewModel: PostDetailViewModel) {
-        
-    }
-    
     var navigationController: UINavigationController
-
+    
+    private let service: PostsServiceProtocol
+    private let imageLoader: ImageLoaderProtocol
+    
     init(
-        navigationController: UINavigationController
+        navigationController: UINavigationController,
+        service: PostsServiceProtocol? = nil,
+        imageLoader: ImageLoaderProtocol? = nil
     ) {
         self.navigationController = navigationController
+        self.service = service ?? PostsService.shared
+        self.imageLoader = imageLoader ?? ImageLoader.shared
+    }
+    
+    func start() {
+        let feedViewModel = FeedViewModel(service: service)
+        let vc = FeedViewController(viewModel: feedViewModel, coordinator: self)
+        navigationController.pushViewController(vc, animated: false)
+    }
+    
+    func showPostDetail(for viewModel: PostDetailViewModel) {
+        let detailVC = PostDetailViewController()
+        navigationController.pushViewController(detailVC, animated: true)
     }
 }
